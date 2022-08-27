00ffbecde36585f4faf74d7ed27397d4af610c8f
Joe Driver <josephdriver@Joes-MacBook-Pro-2.local>
2022-08-27 14:51:23 +1000

    initial commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>
+        Home Screen
+        <Icon name="rocket" size={30} color="#900" />
+      </Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }}
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
